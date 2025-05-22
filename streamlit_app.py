e0853351bfee4fcf9cb5a41858447e1e20f578ad
tora0807 <rarapon20090807@gmail.com>
2025-05-22 08:08:10 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 # streamlit_template
+import streamlit as st
 
-st.title("あいうえお“)
+st.title("あいうえお")
 
 st.write("aiueo")
 
 user_name=st.text_input("名前を入力してください")
 
-st.header("あなたの名前は“＋st(user_name)+"です")
+st.header("あなたの名前は"+st(user_name)+"です")
 
-h=st.number_input("身長を入力してください(ｍ)",value=1.70)
+h=st.number_input("身長を入力してください(m)",value=1.70)
 w=st.number_input("体重を入力してください（kg)",value=70)
 
 bmi=w/(h**2)
