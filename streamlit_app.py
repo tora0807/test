b276993cd4d87eda286cca65e35e2015d06c0d67
tora0807 <rarapon20090807@gmail.com>
2025-05-22 08:08:29 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write("aiueo")
 
 user_name=st.text_input("名前を入力してください")
 
-st.header("あなたの名前は"+st(user_name)+"です")
+st.header("あなたの名前は"+str(user_name)+"です")
 
 h=st.number_input("身長を入力してください(m)",value=1.70)
 w=st.number_input("体重を入力してください（kg)",value=70)
